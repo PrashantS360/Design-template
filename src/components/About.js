dbddc6eb484e3a99179defdd87583fe6029d8961
Prashant Singh <prashantsingh5aug@gmail.com>
2023-06-05 23:22:27 +0530

    Initial Commit

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,8 @@ const About = () => {
             <img src={about} alt="background" className='w-full' />
             <h2 className='font-bold text-center text-5xl font-heading mt-5 mb-2'>India&#39;s Leading Creative Platform</h2>
             <p className='font-para text-lg text-center font-extralight text-gray-600'>Experience hassle free bookings, Trusted by 2500+ Customers</p>
-            <input type="text" className='outline-none border-2 border-black mt-5 px-5 py-2 rounded-full min-w-[55vw] text-center sm:-ml-28' placeholder='Search service, blog and many more...' />
-            <button className='py-2 px-5 min-w-[17vw] border-2 border-black rounded-full max-w-[165px] bg-black text-white relative -top-[2.7rem] left-[24vw] '>Search</button>
+            <input type="text" className='outline-none border-2 border-black mt-5 px-5 py-2 rounded-full min-w-[55%] text-center sm:-ml-28' placeholder='Search service, blog and many more...' />
+            <button className='py-2 px-5 min-w-[17%] border-2 border-black rounded-full max-w-[165px] bg-black text-white relative -top-[2.7rem] left-[24%] '>Search</button>
 
             <div className='flex justify-evenly flex-wrap'>
                 <AboutCard imgLink={card1} heading={"Verified Reviews"} para={"For verified reviews"} />
